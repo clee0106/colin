5d99eb4a6d3c5b94a9aa2d40e02d0e918703afa7
Colin Lee <clee111@student.cscc.edu>
2026-09-23 11:23:00 -0400

    Update project1.py

--- project1.py
+++ project1.py
@@ -3,40 +3,47 @@
 games = []
 
 print("Welcome to the Video Game Backlog Tracker!")
-print("\nMenu")
-print("1. Add a game")
-print("2. View games")
-print("3. Search for a game")
-print("4. Mark a game as completed")
-print("5. Rate a game")
-print("6. Show statistics")
-print("7. Quit")
-
-choice = input("Enter your choice: ")
-if choice == "1":
-  title = input("Enter the game title: ")
-  genre = input("Enter the game genre: ")
-  status = input("Enter the game status: ")
+while True:
+  print("\nMenu")
+  print("1. Add a game")
+  print("2. View games")
+  print("3. Search for a game")
+  print("4. Mark a game as completed")
+  print("5. Rate a game")
+  print("6. Show statistics")
+  print("7. Quit")
   
-  game = {
-    "title": title,
-    "genre": genre,
-    "status": status
-  }
-  games.append(game)
-
-  print(title, "was added to your backlog!")
-elif choice == "2":
-  print("View games selected")
-elif choice == "3":
-  print("Search for a game selected")
-elif choice == "4":
-  print("Mark a game as completed selected")
-elif choice == "5":
-  print("Rate a game selected")
-elif choice == "6":
-  print("Show statistics selected")
-elif choice == "7":
-  print("Goodbye!")
-else:
-  print("Invalid choice")
+  choice = input("Enter your choice: ")
+  if choice == "1":
+    title = input("Enter the game title: ")
+    genre = input("Enter the game genre: ")
+    status = input("Enter the game status: ")
+    
+    game = {
+      "title": title,
+      "genre": genre,
+      "status": status
+    }
+    games.append(game)
+  
+    print(title, "was added to your backlog!")
+  elif choice == "2":
+      if len(games) == 0:
+        print("No games in your backlog.")
+      else:
+        for game in games:
+          print(game["title"], "-", game["genre"], "-", game["status"])
+  elif choice == "3":
+    print("Search for a game selected")
+  elif choice == "4":
+    print("Mark a game as completed selected")
+  elif choice == "5":
+    print("Rate a game selected")
+  elif choice == "6":
+    print("Show statistics selected")
+  elif choice == "7":
+    print("Goodbye!")
+    break
+    
+  else:
+    print("Invalid choice")
